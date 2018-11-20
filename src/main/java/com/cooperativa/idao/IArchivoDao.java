3c0dec63fe1af1af52db7fdd8bd7845ff6f88247
Felipe Torrejon <felipetorrejon@live.cl>
2018-11-20 18:33:17 -0300

    modificacion argumentos metodos eliminarPrograma, eliminarArchivo

--- src/main/java/com/cooperativa/idao/IArchivoDao.java
+++ src/main/java/com/cooperativa/idao/IArchivoDao.java
@@ -22,6 +22,6 @@ public interface IArchivoDao {
   public boolean modificarPrograma(String idArchivo, int indicePrograma, Programa programa);
   public boolean modificarAudio(String idArchivo, int indicePrograma, int indiceAudio, Audio audio);
   public boolean eliminarArchivo(String archivo);
-  public boolean eliminarPrograma(String idArchivo, int indicePrograma);
-  public boolean eliminarAudio(String idArchivo, int indicePrograma, int indiceAudio);
+  public boolean eliminarPrograma(String idArchivo, String indicePrograma);
+  public boolean eliminarAudio(String idArchivo, String indicePrograma, String indiceAudio);
 }
